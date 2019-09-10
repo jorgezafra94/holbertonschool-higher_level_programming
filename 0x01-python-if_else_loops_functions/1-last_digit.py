179c64a680b10792fc9cb3f16fb6252093d78227
jorgezafra94 <947@holbertonschool.com>
2019-09-10 17:41:20 +0000

    [fix] changing messages

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:d}".format(number), end=" ")
+print("Last digit of", end=" ")
 if number < 0:
-    number = number * (-1)
-    last = (number % 10) * (-1)
-    number = number * (-1)
+    last = (number % -10)
 else:
     last = number % 10
 if last > 5:
-    print("is {:d} and is greater than 5".format(last))
+    print("{:d} is {:d} and is greater than 5".format(number, last))
 elif last < 6 and last != 0:
-    print("is {:d} and is less than 6 and not 0".format(last))
+    print("{:d} is {:d} and is less than 6 and not 0".format(number, last))
 else:
-    print("is {:d} and is 0".format(last))
+    print("{:d} is {:d} and is 0".format(number, last))
