a14b59a09496fdb6354920e82895766a72b859d8
jorgezafra94 <947@holbertonschool.com>
2019-09-10 18:19:54 +0000

    change to uppercase everything

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    tam = len(str)
+    for i in range(0, tam):
+        asci = ord(str[i])
+        if asci >= 97 and asci <= 122:
+            asci = asci - 32
+        if i < (tam - 1):
+            print("{:c}".format(asci), end="")
+        else:
+            print("{:c}".format(asci))
