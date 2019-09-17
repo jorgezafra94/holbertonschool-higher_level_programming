900872f97cac05b84cc46a987d3f96dd7852c604
jorgezafra94 <947@holbertonschool.com>
2019-09-17 17:36:45 +0000

    [feature] function that replace the value of a specified position

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+    elif idx >= len(my_list):
+        return (my_list)
+    else:
+        for i in range(len(my_list)):
+            if i == idx:
+                del my_list[i]
+                my_list.insert(idx, element)
+                return (my_list)
+        return (None)
