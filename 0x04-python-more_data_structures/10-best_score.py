69a6c921931e0cd53a98876ae7f24c0cc475f0a7
jorgezafra94 <947@holbertonschool.com>
2019-09-19 17:16:34 +0000

    [Update] if no score found return None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,10 @@ def best_score(a_dictionary):
     big = 0
     name = ""
     if a_dictionary is None:
-        return None
+        return 
     for element in a_dictionary:
+        if len(a_dictionary) == 0:
+            return None
         if num == 0:
             big = a_dictionary[element]
         if big < a_dictionary[element]:
