b68eeb333f181817f09955f36a71cf47a370c616
jorgezafra94 <947@holbertonschool.com>
2019-09-19 17:38:36 +0000

    [fix] using max function

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    num = 0
-    big = 0
-    name = ""
     if not a_dictionary:
         return None
-    for element in a_dictionary:
-        if num == 0:
-            big = a_dictionary[element]
-        if big < a_dictionary[element]:
-            big = a_dictionary[element]
-            name = (list(a_dictionary))[num]
-        num = num + 1
-    return (name)
+    return (max(a_dictionary))
