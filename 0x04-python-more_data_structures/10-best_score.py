5877c13eade0dab1afed58ef3ae38cd44013f7e0
jorgezafra94 <947@holbertonschool.com>
2019-09-19 17:11:26 +0000

    [feature] who has the biggest score in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    num = 0
+    big = 0
+    name = ""
+    if a_dictionary is None:
+        return None
+    for element in a_dictionary:
+        if num == 0:
+            big = a_dictionary[element]
+        if big < a_dictionary[element]:
+            big = a_dictionary[element]
+            name = (list(a_dictionary))[num]
+        num = num + 1
+    return (name)
