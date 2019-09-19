e0b3cecea62c2d780daa6441e759c90cace50b6e
jorgezafra94 <947@holbertonschool.com>
2019-09-19 18:05:38 +0000

    [fix] change all logic again do it manually

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    num = 0
+    maxi = 0
     if not a_dictionary:
         return None
-    return (max(a_dictionary.keys()))
+    for element in a_dictionary:
+        if num == 0:
+            maxi = a_dictionary[element]
+        if maxi < a_dictionary[element]:
+            maxi = a_dictionary[element]
+        num = num + 1
+    for element in a_dictionary:
+        if maxi == a_dictionary[element]:
+            return (element)
