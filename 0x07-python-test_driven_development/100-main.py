bb4103ae68c6586acc364e33a9c65d91388e261c
jorgezafra94 <947@holbertonschool.com>
2019-10-04 20:54:08 +0000

    main file

---
+++ 0x07-python-test_driven_development/100-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+matrix_mul = __import__('100-matrix_mul').matrix_mul
+
+print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+print(matrix_mul([[1, 2, 5]], [[3, 4], [5, 6], [10, 10]]))
+print(matrix_mul([[1, 4, 7], [2, 5, 8], [3, 6, 9]], [[1, -1, 2], [2, -1, 2], [3, -3, 0]]))
