24f200002df7b52a4e0f57cce5f0f80fd559f629
jorgezafra94 <947@holbertonschool.com>
2019-10-16 21:47:02 +0000

    [feature] method that replace key values

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+class Student
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is not list:
+            return (self.__dict__)
+        else:
+            new = {}
+            ob = self.__dict__
+            for i in attrs:
+                for ele in ob:
+                    if (i == ele):
+                        new[i] = ob[i]
+            return (new)
+
+    def reload_from_json(self, json):
+        ob = self.__dict__
+        for i in json:
+            for ele in ob:
+                if (ele == i):
+                    setattr(self, i, json[i])
