4979613f56f28d56c5090e84037553dbccc8cfe0
jorgezafra94 <947@holbertonschool.com>
2019-10-16 13:24:20 +0000

    [fix] change class BaseGeometry by import the module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,22 +1,5 @@
 #!/usr/bin/python3
-"""
-class BaseGeometry
-"""
-
-
-class BaseGeometry:
-    def __init__(self):
-        pass
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(name + ' must be an integer')
-        if value <= 0:
-            raise ValueError(name + ' must be greater than 0')
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
