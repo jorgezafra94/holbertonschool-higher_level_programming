bbd38d6cdc8c9f367ee4beac31fcd4821468fc0f
jorgezafra94 <947@holbertonschool.com>
2019-10-16 20:32:53 +0000

    [feature] obj dict

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -5,8 +5,4 @@ create a json from a object of a class
 
 
 def class_to_json(obj):
-    import json
-
-    new = json.dumps(obj.__dict__)
-    new = json.loads(new)
-    return (new)
+    return (obj.__dict__)
