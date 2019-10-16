f772e5e119bc91868e6c660a115ab3e4008e4829
jorgezafra94 <947@holbertonschool.com>
2019-10-16 16:16:55 +0000

    [feature] open, read and print content of a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ Read and print the content of a file
 
 
 def read_file(filename=""):
-    with open(filename, 'r') as f:
+    with open(filename) as f:
         read_data = f.read()
         print(read_data, end="")
