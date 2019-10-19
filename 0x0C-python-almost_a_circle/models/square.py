3c701d23d0f3c31a8e77f3c7f1dbba4e5958b194
jorgezafra94 <947@holbertonschool.com>
2019-10-19 00:06:51 +0000

    [update] adding decorers for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,8 +17,25 @@ class Square(Rectangle):
     methods:
         * __str__: print ecuation
     """
+    elem = 0
+
     def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value <= 0:
+                raise ValueError("width must be > 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("width must be an integer")
 
     def __str__(self):
         args = [self.id, self.x, self.y, self.width]
