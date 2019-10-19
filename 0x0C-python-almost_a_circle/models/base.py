7eb9260b3eff345b14d315181ba961ac19c4b92e
jorgezafra94 <947@holbertonschool.com>
2019-10-19 18:50:49 +0000

    [Update] add classmethod save_into_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
         else asign to self.id the value of id
 
         * to_json_string: return an object in json format
+        * save_to_fie: save in a fil the dicts of each instance passed
         """
         if id is None:
             Base.__nb_objects += 1
@@ -25,8 +26,23 @@ class Base:
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         else:
             return(json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        new = []
+        result = ""
+        with open(filename, 'w') as fd:
+            if list_objs is None:
+                result = cls.to_json_string(new)
+            else:
+                for elem in list_objs:
+                    new.append(elem.to_dictionary())
+                result = cls.to_json_string(new)
+            fd.write(result)
