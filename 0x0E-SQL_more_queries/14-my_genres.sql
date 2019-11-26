d9c918b728ba736ac6431415ddefe3c18c1a8a1b
jorgezafra94 <947@holbertonschool.com>
2019-11-26 23:36:13 +0000

    [feature] 2 JOINS 3 tables

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,7 @@
 -- double JOIN using 3 different tables
 -- the idea is always get relations table-table and in that way
 -- get the information that we need
--- in this example the connection is tv_show_genres.show_id because is used
--- in both JOINs
+-- the important thing is relate both JOINs
 SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
