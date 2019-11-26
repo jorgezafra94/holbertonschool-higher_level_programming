144649ddeec93bc90c8b95ca92e59245880082a3
jorgezafra94 <947@holbertonschool.com>
2019-11-26 23:16:23 +0000

    [feature] JOIN 3 Tables keeping the connections

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,8 +3,8 @@
 -- get the information that we need
 -- in this example the connection is tv_show_genres.show_id because is used
 -- in both JOINs
--- we dont put order by because by default use the ascendent form
 SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter";
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_show.tittle ASC;
