abad0cb106e706ddf3b0697bc3b7379ebc91da43
jorgezafra94 <947@holbertonschool.com>
2019-11-27 00:32:39 +0000

    [feature] using sub queries and asign a name in this case (dexter)

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,5 +7,5 @@ LEFT JOIN
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = "Dexter") dexter ON tv_genres.name = dexter.name
-WHERE dexter.name IS NULL;
+WHERE dexter.name IS NULL
 ORDER BY tv_genres.name ASC;
