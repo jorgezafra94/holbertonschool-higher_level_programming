2f9db5d706bbc0f2abe91d3733806acf0fa514b8
jorgezafra94 <947@holbertonschool.com>
2019-11-27 00:01:32 +0000

    [feature] using Right JOIN in order to get all of the new table

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@
 SELECT tv_shows.title, tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
