070bf8d71ce62097f3762b5a90a0daa6dad7292a
jorgezafra94 <947@holbertonschool.com>
2019-12-13 20:30:54 +0000

    [feature] ORM to get info from a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+here we are going to connect to a database and realize a
+query
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         port=3306, passwd=sys.argv[2], db=sys.argv[3])
+    query = db.cursor()
+    query.execute("SELECT * FROM states; ORDER BY id ASC;")
+    output = query.fetchall()
+    for i in output:
+        print(i)
