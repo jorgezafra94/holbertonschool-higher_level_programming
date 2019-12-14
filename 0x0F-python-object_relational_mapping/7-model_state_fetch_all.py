a489ec180caf536d2fdfc35bb3f4ed5ace884c60
jorgezafra94 <947@holbertonschool.com>
2019-12-14 15:43:48 +0000

    [feature] using quey in table states from class State

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    # lets create a Session object
+    ses = Session()
+    # get a list with all the objects order by id type State
+    output = ses.query(State).order_by(State.id)
+    for elem in output:
+        print("{}: {}".format(elem.id, elem.name))
