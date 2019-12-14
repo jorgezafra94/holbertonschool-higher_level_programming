2d0d3e79ff097a0209b39a989bdd6fc88a954079
jorgezafra94 <947@holbertonschool.com>
2019-12-14 16:54:30 +0000

    [feature] getting info using MySQLdb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,16 @@
 here we are going to connect to a database and realize a
 query
 """
+import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         port=3306, passwd=sys.argv[2], db=sys.argv[3])
+                         port=3306, passwd=sys.argv[2], db=sys.argv[3],
+                         charset="utf8")
     query = db.cursor()
-    query.execute("SELECT * FROM states; ORDER BY id ASC;")
+    query.execute("SELECT * FROM states ORDER BY id ASC")
     output = query.fetchall()
     for i in output:
         print(i)
