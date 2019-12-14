af36c8c5a3c9134c02f6b64e3f64f50ed2db5e1b
jorgezafra94 <947@holbertonschool.com>
2019-12-14 18:07:45 +0000

    [feature] insert a new element in table

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+ORM queries that modify the databse
+
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    # lets create a Session object
+    ses = Session()
+    # adding a new element in the table
+    Louisiana = State(name='Louisiana', id=6)
+    ses.add(Louisiana)
+    print(ses.query(State).count())
+    ses.commit()
+    ses.close()
