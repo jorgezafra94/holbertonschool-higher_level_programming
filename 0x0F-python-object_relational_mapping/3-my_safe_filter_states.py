e8d390313450e9ceb377fee5a62a68e686e27159
jorgezafra94 <947@holbertonschool.com>
2019-12-14 17:04:56 +0000

    [feature] safe for to use variables

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,16 +3,19 @@
 here we are going to connect to a database and realize a
 query
 """
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         port=3306, passwd=sys.argv[2], db=sys.argv[3])
+                         port=3306, passwd=sys.argv[2], db=sys.argv[3],
+                         charset="utf8")
     query = db.cursor()
     x = sys.argv[4]
-    query.execute("SELECT * FROM states WHERE name=%s;\
-                  ORDER BY id ASC;", (sys.argv[4],))
+    query.execute("SELECT * FROM states WHERE name=%s\
+                  ORDER BY id ASC", (sys.argv[4],))
     output = query.fetchall()
     for i in output:
         print(i)
+    query.close()
+    db.close()
