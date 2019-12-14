0a14b6a5db091ddeb234d974756a4f999fc2767a
jorgezafra94 <947@holbertonschool.com>
2019-12-14 17:09:10 +0000

    [feature] print cities of specified state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,18 +3,19 @@
 here we are going to connect to a database and realize a
 query
 """
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         port=3306, passwd=sys.argv[2], db=sys.argv[3])
+                         port=3306, passwd=sys.argv[2], db=sys.argv[3],
+                         charset="utf8")
     query = db.cursor()
     state = sys.argv[4]
     query.execute("SELECT cities.id, cities.name, states.name FROM cities\
                  LEFT JOIN states ON states.id = cities.state_id\
-                 WHERE states.name = %s;\
-                 ORDER BY cities.id ASC;", (sys.argv[4],))
+                 WHERE states.name = %s\
+                 ORDER BY cities.id ASC", (sys.argv[4],))
     output = query.fetchall()
     aux = ""
     result = ""
