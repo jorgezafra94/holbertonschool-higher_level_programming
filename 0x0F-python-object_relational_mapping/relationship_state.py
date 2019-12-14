8b5278ffd3f0d2d0c7d26a9bacd643fc676c4a8c
jorgezafra94 <947@holbertonschool.com>
2019-12-14 21:48:36 +0000

    [feature] Class with relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+class to use in call of ORM
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+
+    class that is going to run in the ORM
+
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", cascade='delete', backref="state")
