286bd583db7977ae1972d2d1b66aaffcff379099
= <=>
2020-01-06 21:22:48 +0000

    [feature] find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,7 +10,11 @@ def find_peak(list_of_integers):
         return (None)
     lista = list_of_integers
     for i in range(len(lista)):
+        if i == 0:
+            aux = lista[i]
         if i != 0 and i != len(lista) - 1:
+            if lista[i] >= aux:
+                aux = lista[i]
             if lista[i] >= lista[i - 1] and lista[i] >= lista[i + 1]:
                 return(lista[i])
-    return (None)
+    return (aux)
