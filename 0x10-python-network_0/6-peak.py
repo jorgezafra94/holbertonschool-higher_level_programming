ce8ecbc805f87c71f9b4c6b908b3369ab5940b3c
= <=>
2020-01-06 22:03:29 +0000

    [feature] get the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,12 +3,30 @@
 """
 
 
+def recursion(begin, last, l):
+    """ search
+    """
+    mid = (begin + last)
+
+    if (mid % 2 == 0):
+        m = int(mid / 2)
+    else:
+        m = int((mid - 1) / 2)
+    if (m == 0 or l[m] >= l[m - 1]) and (m == last - 1 or l[m] >= l[m + 1]):
+        return (l[m])
+    elif (m > 0 and l[m] < l[m - 1]):
+        return (recursion(begin, m, l))
+    elif (m < last - 1 and l[m] < l[m + 1]):
+        return (recursion(m, last, l))
+    else:
+        return(None)
+
+
 def find_peak(list_of_integers):
     """ function that get the peak from a list
     """
     if not list_of_integers:
         return (None)
+
     l = list_of_integers
-    for i in range(len(l)):
-        if (i == 0 or l[i] >= l[i + 1]) and (i == len(l) - 1 or l[i] >= l[i - 1]):
-            return (l[i])
+    return (recursion(0, len(l), l))
