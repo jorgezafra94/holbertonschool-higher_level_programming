72ed8fe3e4ad3f56bb526d9ae3c1f078292c6966
= <=>
2020-01-06 21:20:18 +0000

    [feature] find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,19 +8,9 @@ def find_peak(list_of_integers):
     """
     if not list_of_integers:
         return (None)
-    if len(list_of_integers) < 3:
-        aux = list_of_integers[0]
-        for i in range(1, len(list_of_integers)):
-            if list_of_integers[i] > aux:
-                return(list_of_integers[i])
-            else:
-                return(aux)
-    L = list_of_integers
-    for i in range(1, len(L)):
-        if (L[i] > L[i-1] and L[i] > L[i+1]):
-            return(L[i])
-    aux = L[0]
-    for i in range(1, len(L)):
-        if L[i] > aux:
-            aux = L[i]
-    return (aux)
+    lista = list_of_integers
+    for i in range(len(lista)):
+        if i != 0 and i != len(lista) - 1:
+            if lista[i] >= lista[i - 1] and lista[i] >= lista[i + 1]:
+                return(lista[i])
+    return (None)
