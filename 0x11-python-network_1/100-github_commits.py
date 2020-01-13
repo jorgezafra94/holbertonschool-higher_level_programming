121af4bc69005ed01838524315d6f97eb854d1b2
= <=>
2020-01-13 22:11:13 +0000

    [feature] getting first 10 commits from repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" get the first 10 commits from repo
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+    owner = sys.argv[1]
+    repo = sys.argv[2]
+    API = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    r = requests.get(API)
+    # the commits are in order from new to old
+    for i in range(10):
+        commit = r.json()[i]
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print("{} {}".format(sha, author))
