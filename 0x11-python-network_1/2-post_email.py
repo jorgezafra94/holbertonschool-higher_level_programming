4aa44f23547d7d6a73152847bf20b096bcded704
= <=>
2020-01-13 17:03:55 +0000

    [feature] POST a email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" POST a variable with key
+"""
+
+
+import sys
+import urllib.request
+
+values = {'email': '{}'.format(sys.argv[2])}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+# data should be bytes
+try:
+    with urllib.request.urlopen(sys.argv[1], data) as response:
+        # if we use data we are calling POST method
+        for i in response:
+            # here we get the answer
+            print(i.decode('utf-8'))
+except urllib.error.URLError as e:
+    print(e.reason)
