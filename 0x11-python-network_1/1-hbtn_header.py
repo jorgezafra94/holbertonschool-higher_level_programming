68ee0052706df812a55843416b0b928dc44021c9
= <=>
2020-01-13 17:55:27 +0000

    [fix] use of if '__name__'

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ get headers from web-site
 """
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-import urllib.request
-import sys
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    headers = response.info()
-    print(headers.get('X-Request-Id'))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        headers = response.info()
+        print(headers.get('X-Request-Id'))
