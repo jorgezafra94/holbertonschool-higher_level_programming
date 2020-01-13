2897538627b7a5e2022fd53b76a5ec045d899245
= <=>
2020-01-13 16:09:04 +0000

    [feature] infomation about result gotten

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" use urllib to get information from a web-site
+"""
+
+import urllib.request
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    result = response.read()
+ty = type(result)
+con = result
+con_u = result.decode('utf-8')
+print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 \
+content: {}".format(ty, con, con_u))
