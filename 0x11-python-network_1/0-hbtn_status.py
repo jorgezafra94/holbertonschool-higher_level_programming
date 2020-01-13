67510e8ca0ffb6188aa73950511eaa828255abd7
= <=>
2020-01-13 17:56:52 +0000

    [fix] if '__name__'

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ use urllib to get information from a web-site
 """
+if __name__ == "__main__":
+    import urllib.request
 
-import urllib.request
-
-req = urllib.request.Request('https://intranet.hbtn.io/status')
-try:
-    with urllib.request.urlopen(req) as response:
-        result = response.read()
-    ty = type(result)
-    con = result
-    con_u = result.decode('utf-8')
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 \
-content: {}".format(ty, con, con_u))
-except urllib.error.URLError as e:
-    print(e.reason)
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    try:
+        with urllib.request.urlopen(req) as response:
+            result = response.read()
+        ty = type(result)
+        con = result
+        con_u = result.decode('utf-8')
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 \
+        content: {}".format(ty, con, con_u))
+    except urllib.error.URLError as e:
+        print(e.reason)
