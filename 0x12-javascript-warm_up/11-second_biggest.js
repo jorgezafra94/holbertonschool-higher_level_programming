7d47bf246f50441c8c96cd15f8f828178e19a198
= <=>
2020-02-04 18:15:40 +0000

    [fix] change the sort because the sort order alphabetically

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,10 +8,29 @@ slice get a new array from the position selected
 */
 let x = 0;
 const list = process.argv.slice(2);
+const list2 = [];
 x = process.argv.length;
 if (x <= 2 || x === 3) {
-  console.log('0');
+  console.log(0);
 } else {
-  list.sort();
-  console.log(list[list.length - 2]);
+  let count = 0;
+  /* get the max value */
+  let value = Math.max.apply(null, list);
+  for (; count < list.length; count++) {
+    if (list[count] === value) {
+      delete list[count];
+      break;
+    }
+  }
+  /* create the new list without the empty element */
+  count = 0;
+  for (; count < list.length; count++) {
+    if (list[count]) {
+      list2.push(list[count]);
+    }
+  }
+  console.log(list2);
+  /* once we delete the biggest we get the second one */
+  value = Math.max.apply(null, list2);
+  console.log(value);
 }
