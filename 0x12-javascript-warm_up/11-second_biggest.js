2bfacc43594c68886f9692699c865c16bc052339
= <=>
2020-02-04 16:21:03 +0000

    [feature] get the second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* process.argv return the arguments
+the first argument is always node
+the second is the executable file
+the third is the first argument
+
+slice get a new array from the position selected
+*/
+let x = 0;
+const list = process.argv.slice(2);
+x = process.argv.length;
+if (x <= 2 || x === 3) {
+  console.log(0);
+} else {
+  list.sort();
+  console.log(list[list.length - 2]);
+}
