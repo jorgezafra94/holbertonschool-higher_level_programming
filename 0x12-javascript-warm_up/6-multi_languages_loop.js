9a90247f12f0e471f9b5c0d05e69114142029c59
= <=>
2020-02-04 15:32:46 +0000

    [feature] using a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'Javascript is amazing';
+const array = [first, second, third];
+let count = 0;
+for (; count < array.length; count++) {
+  console.log(array[count]);
+}
