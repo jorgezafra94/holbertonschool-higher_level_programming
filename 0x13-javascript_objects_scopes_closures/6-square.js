40a1558dcfd3a2b5fa62dbedd072fb6deba4f58d
jorgezafra94 <947@holbertonschool.com>
2020-02-10 21:33:17 +0000

    [feature] add method to child

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 /* inheritance in JS */
 const Rectangle = require('./4-rectangle');
-// call the inheritance
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (!c) {
-      c = 'X';
-    }
+  charPrint (c = 'X') {
     let row = '';
     let count = 0;
     for (; count < this.width; count++) {
