306bd38aa63de0dc15f6b104a9078b589e0523ab
jorgezafra94 <947@holbertonschool.com>
2020-02-10 21:28:31 +0000

    [feature] add method to child and give a predefine value

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/* inheritance in JS */
+const Rectangle = require('./4-rectangle');
+// call the inheritance
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    let row = '';
+    let count = 0;
+    for (; count < this.width; count++) {
+      row = row + c;
+    }
+    for (count = 0; count < this.height; count++) {
+      console.log(row);
+    }
+  }
+};
