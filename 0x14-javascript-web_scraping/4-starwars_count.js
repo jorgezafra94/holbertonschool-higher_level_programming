5a2876ac8fbc5caa6f88a0befb03f116c3e05cf4
= <=>
2020-02-11 16:13:41 +0000

    [feature] go through json body

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* module request */
+const request = require('request');
+const character = 'https://swapi.co/api/people/18/';
+let counter = 0;
+
+const page = process.argv[2];
+request(page, function (error, status, body) {
+  if (error) {
+    console.error(error);
+  }
+  const lista = JSON.parse(body);
+  for (let i = 0; i < lista.results.length; i++) {
+    for (let j = 0; j < lista.results[i].characters.length; j++) {
+      if (lista.results[i].characters[j] === character) {
+        counter += 1;
+      }
+    }
+  }
+  console.log(counter);
+});
