036d5213fce06d1cd607c7d8a7aa64644c11c7fb
= <=>
2020-02-11 15:03:45 +0000

    [feature] using buffer or error with fsreadfile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  console.log(data)
-})
+  console.log(data);
+});
