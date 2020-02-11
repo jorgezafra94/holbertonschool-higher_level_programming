7fe11f61b005016c5b24daea999ec6de19c88204
= <=>
2020-02-11 18:12:17 +0000

    [fix] change if for search

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 /* module request */
 const request = require('request');
-const character = 'https://swapi.co/api/people/18/';
 let counter = 0;
 const page = process.argv[2];
 request(page, function (error, status, body) {
   if (error) {
     console.error(error);
-  } else if (status.statusCode === 200) {
-    const lista = JSON.parse(body);
-    for (let i = 0; i < lista.results.length; i++) {
-      for (let j = 0; j < lista.results[i].characters.length; j++) {
-        if (lista.results[i].characters[j] === character) {
-          counter += 1;
-        }
+  }
+  const lista = JSON.parse(body);
+  for (let i = 0; i < lista.results.length; i++) {
+    for (let j = 0; j < lista.results[i].characters.length; j++) {
+      if (lista.results[i].characters[j].search('/18/') > 0) {
+        counter += 1;
       }
     }
-    console.log(counter);
   }
+  console.log(counter);
 });
