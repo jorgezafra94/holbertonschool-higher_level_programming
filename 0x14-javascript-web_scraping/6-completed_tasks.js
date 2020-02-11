5ad493f3f1ee6ed6ad6d986093703091176e999f
= <=>
2020-02-11 18:41:25 +0000

    [fix] redoing everything

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,33 +1,22 @@
 #!/usr/bin/node
 /* module request */
 const request = require('request');
-let counter = 0;
-const users = [];
-const number = [];
 const page = process.argv[2];
+const dicti = {};
 request(page, function (error, status, body) {
   if (error) {
     console.error(error);
   } else {
     const lista = JSON.parse(body);
-    for (const user of Object.values(lista)) {
-      users.push(user.userId);
-    }
-    let keys = new Set(users);
-    keys = Array.from(keys);
-    for (const id of Object.values(keys)) {
-      counter = 0;
-      for (const obj of Object.values(lista)) {
-        if (id === obj.userId && obj.completed === true) {
-          counter++;
+    for (const task of Object.values(lista)) {
+      if (task.completed) {
+        if (dicti[task.userId]) {
+          dicti[task.userId] += 1;
+        } else {
+          dicti[task.userId] = 1;
         }
       }
-      number.push(counter);
     }
-    const result = {};
-    for (let i = 0; i < keys.length; i++) {
-      result[keys[i]] = number[i];
-    }
-    console.log(result);
+    console.log(dicti);
   }
 });
